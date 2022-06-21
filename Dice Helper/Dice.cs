e1fc5204d158b0fac0b82fa42585273a012bd5e8
Liam Powers <liam.t.powers@gmail.com>
2022-06-21 17:59:20 -0400

    Add project files.

---
+++ Dice Helper/Dice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice_Helper
+{
+    internal class Dice
+    {
+        public String Roll(String input)
+        {
+            Random random = new Random();
+            String[] rollArray = new String[2];
+            rollArray = input.Split('d');
+            int quantity = Int32.Parse(rollArray[0]);
+            int die = Int32.Parse(rollArray[1]);
+
+            String results = "";
+
+            for (int i = 0; i < quantity; i++)
+            {
+                if (i == 0)
+                {
+                    results += random.Next(1, die + 1);
+                }
+                else
+                {
+                    results += ", " + random.Next(1, die + 1);
+                }
+            }
+            return results;
+        }
+    }
+}
---
+++ Dice Helper/Program.cs
@@ -0,0 +1,28 @@
+﻿using Dice_Helper;
+
+bool loop = true;
+Random random = new Random();
+while (loop)
+{
+    Console.WriteLine("What would you like to do?");
+    String? input = Console.ReadLine();
+    if (input == null) input = "";
+    input = input.ToLower();
+    String[] inputArray = input.Split(' ');
+    Dice dice = new Dice();
+
+    switch (inputArray[0])
+    {
+        case ("exit"): 
+            loop = false;
+            break;
+        case ("roll"):
+            Console.WriteLine(dice.Roll(inputArray[1]));
+            Console.ReadLine();
+            Console.Clear();
+            break;
+        default: 
+            Console.WriteLine("Invalid input, try again."); 
+            break;
+    }
+}
